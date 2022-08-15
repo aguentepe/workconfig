0bc6f89be30bc32d9101bf954404f8cd86e54dcb
Abdullah Güntepe <abdullah@guentepe.com>
2022-08-15 13:40:33 +0200

    autostart: Allow script to be run multiple times

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -2,31 +2,19 @@
 
 ! [ -d "$HOME/.cache/wal" ] && wal-link /usr/share/wallpapers/deepin/Sunrise_by_Alexandre_Godreau.jpg
 
-autostart2.sh
+run() {
+  if ! pgrep -f "$1"
+  then
+    "$@"&
+  fi
+}
 
-[ -f /tmp/autostart.isrunning ] && echo 'autostart.sh: already running' && exit
-touch /tmp/autostart.isrunning
-
-pipewire &
-pipewire-pulse &
-wireplumber &
-# sudo wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
-
-# sudo wakeup_fix
-
-#Start Daemons
-dunst &
-thttpd -p 8457 -d .local/share/www
-# clipboard-polling.sh &
-
-#Startup Programs
-avpn &
-udiskie &
-
-# while true; do
-# 	# xsetroot -name "$(date +'%H:%M:%S')"
-# 	# sleep 2
-# 	[ "$(cat /sys/class/power_supply/BAT0/capacity)" -lt 20 ] &&
-# 		notify-send "Battery Low" -u critical
-# 	sleep 180
-# done
+# autorandr -c
+start-pulseaudio-x11
+xwallpaper --zoom "$HOME/.config/wall.jpg"
+xset b 0 0 0 && xset b off && xset s off && xset -dpms
+run udiskie
+run picom -c -i 1
+run nm-applet
+run cbatticon
+xbacklight =50
